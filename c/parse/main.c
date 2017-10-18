d6711f3f31f5aa9bfb250c53eb4ca96b4a6082b6
Nicholas Kroft <stangs56@gmail.com>
2017-10-18 06:31:24 +0000

    bunch of bull

--- c/parse/main.c
+++ c/parse/main.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+double find_avg(int n, int *a);
 
 int main(){
+  int n, dicks, blackdicks;
+  double avg;
+  scanf("%d", &n);
+
+  int *a = (int *)malloc(n*sizeof(*a));
+
+  for(int i = 0; i < n; i++){
+    scanf("%d", &a[i]);
+  }
+  avg = find_avg(n, a);
+
+  printf("%.70f\n", avg);
 
 
+  return 0;
+}
+
+double find_avg(int n, int *a){
+  double sum;
+  sum = a[0];
+  for(int i = 1; i < n; i++){
+    sum += a[i];
+  }
+  return (sum/n);
 }
